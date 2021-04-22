7e5c10d9e04d66833cc608c4cdac1833537758f1
Paul C <pcutter@cnm.edu>
2021-04-22 15:21:59 -0600

    Initial commit

--- money2text.js
+++ money2text.js
@@ -1,8 +1,54 @@
 'use strict'
 
-function money2text (time) {
-
-
-}
-
+  function money2text (money) {
+    //PARSING THE MONEY (AT THE DECIMAL POINT)
+    let [dollarStr, centStr] = money.split(".");
+    let dollarValue = parseInt(dollarStr);
+    let centValue = parseInt(centStr);
+    //CENTS PORTION - LAST 2 CHARACTERS
+    if (centValue === 0) {
+      return humanize(dollarValue) + " dollars";
+    }
+    //IF 0 DOLLARS => RETURN CENTS STRING
+    return humanize(dollarValue)+" dollars" + " and " + humanize(centValue) +" cents";
+        }
 module.exports = money2text
+
+
+function humanize(num){
+  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+    'seventeen', 'eighteen', 'nineteen'];
+  var tens = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty',
+    'ninety'];
+
+  var numString = num.toString();
+
+  if (num < 0) throw new Error('Negative numbers are not supported.');
+
+  if (num === 0) return 'zero';
+
+  //the case of 1 - 20
+  if (num < 20) {
+    return ones[num];
+  }
+
+  if (numString.length === 2) {
+    return tens[numString[0]] + ' ' + ones[numString[1]];
+  }
+
+  //100 and more
+  if (numString.length === 3) {
+    if (numString[1] === '0' && numString[2] === '0')
+      return ones[numString[0]] + ' hundred';
+    else
+      return ones[numString[0]] + ' hundred and ' + humanize(+(numString[1] + numString[2]));
+  }
+
+  if (numString.length === 4) {
+    var end = +(numString[1] + numString[2] + numString[3]);
+    if (end === 0) return ones[numString[0]] + ' thousand';
+    if (end < 100) return ones[numString[0]] + ' thousand and ' + humanize(end);
+    return ones[numString[0]] + ' thousand ' + humanize(end);
+  }
+}
--- time2text.js
+++ time2text.js
@@ -1,8 +1,109 @@
 'use strict'
-
 function time2text (time) {
-
-
+  // handle edge cases
+  if (time === '00:00') return 'midnight';
+  if (time === '12:00') return 'noon';
+  // process the input
+  let [hourStr, minutesStr] = time.split(":");
+  let hour = parseInt(hourStr);
+  let originalHour = parseInt(hourStr)
+  //let minuts = parseInt(minutesStr);
+  let morningAfternoonEvening = "";
+  // get the ampm
+  // if (hour >= 0 && hour < 12) {
+  //     morningAfternoonEvening = "in the morning";
+  // } else if (hour >=12 && hour <18){
+  //     morningAfternoonEvening = "in the afternoon";
+  // } else {
+  //     morningAfternoonEvening = "in the evening";
+  // }
+  // convert the hour
+  if (hour > 12) {
+    hour -= 12;
+  }
+  // map hours to string format
+  let numberWordMap = {
+    0: "twelve",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen"
+  }
+  let tensMap = {
+    2: "twenty",
+    3: "thirty",
+    4: "fourty",
+    5: "fifty"
+  }
+  hourStr = numberWordMap[hour];
+  if (parseInt(minutesStr) < 20) {
+    //2 scenarios
+    //1 - minutes might be 00, return oh clock
+    if(minutesStr === '00') {
+      return hourStr + " o'clock " + helper(originalHour);
+    }
+    //handle the 5, 10, 15 min cases
+    if(minutesStr === '05') {
+      return 'five past ' + hourStr + " " + helper(originalHour);
+    } else if(minutesStr === '10') {
+      return 'ten past ' + hourStr + " " + helper(originalHour);
+    } else if(minutesStr === '15') {
+      return 'quarter past ' + hourStr + " " + helper(originalHour);
+    }
+    //2 - 01 - 09, return oh + minute
+    if(minutesStr.charAt(0) === '0') {
+      minutesStr = "oh " + numberWordMap[minutesStr.charAt(1)];
+    } else {
+      minutesStr = numberWordMap[minutesStr];
+    }
+  } else {
+    //handle the 20, 30, 40, 45, 50, 55 min cases
+    if(minutesStr === '20') {
+      return 'twenty past ' + hourStr + " " + helper(originalHour);
+    } else if(minutesStr === '30') {
+      return 'half past ' + hourStr + " " + helper(originalHour);
+    } else if(minutesStr === '40') {
+      hourStr = numberWordMap[(hour + 1) % 12];
+      return 'twenty to ' + hourStr + " " + helper(originalHour + 1);
+    } else if(minutesStr === '45') {
+      hourStr = numberWordMap[(hour + 1) % 12];
+      return 'quarter to ' + hourStr + " " + helper(originalHour + 1);
+    } else if(minutesStr === '50') {
+      hourStr = numberWordMap[(hour + 1) % 12];
+      return 'ten to ' + hourStr + " " + helper(originalHour + 1);
+    } else if(minutesStr === '55') {
+      hourStr = numberWordMap[(hour + 1) % 12];
+      return 'five to ' + hourStr + " " + helper(originalHour + 1);
+    }
+    minutesStr = tensMap[minutesStr.charAt(0)] + "-" + numberWordMap[minutesStr.charAt(1)];
+  }
+  // the conversion
+  return hourStr + " " + minutesStr + " " + helper(originalHour);
+}
+function helper(hour) {
+  if (hour >= 0 && hour < 12) {
+    return "in the morning";
+  } else if (hour >=12 && hour <18){
+    return "in the afternoon";
+  } else {
+    return "in the evening";
+  }
 }
 
 module.exports = time2text
+
